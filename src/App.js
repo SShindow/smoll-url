226eb0b3d17f9f4799fead20fd6d7f579af4bb55
Hai Cao Xuan <hia.cao.2001@gmail.com>
2023-08-15 04:15:37 +0700

    render complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Form from './components/form/Form';
+import Form from "./components/Form";
 
 function App() {
   return (
@@ -10,8 +10,8 @@ function App() {
       <div className="auth-wrapper">
         <div className="auth-inner">
           <Routes>
-            <Route exact path='/' element={<Form/>} />
-            <Route path="/app" element={<Form/>} />
+            <Route path="/" element={<Form />} />
+            <Route path="/app" element={<Form />} />
           </Routes>
         </div>
       </div>
