94424b519211fd21083bed7e52ea88a64c15a330
Hai Cao Xuan <hia.cao.2001@gmail.com>
2023-08-18 14:06:50 +0700

    activate venv and run on server

--- server/app/main.py
+++ server/app/main.py
@@ -4,23 +4,23 @@ from firebase_admin import db
 import os
 
 cred_obj = firebase_admin.credentials.Certificate('./ServiceAccountKey.json')
-default_app = firebase_admin.initalize_app(cred_obj, {
-    'databaseURL': 'http://https://smoll-url-default-rtdb.asia-southeast1.firebasedatabase.app/'
-})
+default_app = firebase_admin.initialize_app(cred_obj,  {
+	'databaseURL': 'https://smoll-url-default-rtdb.asia-southeast1.firebasedatabase.app/'
+	})
 
-app = Flask(__name__, static_folder='./build/static', template_folder="./build")
+app = Flask(__name__, static_folder='./build/static', template_folder="./build" )
 
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return redirect("./app")
+    return redirect("/app")
 
-@app.route("./app")
+@app.route("/app")
 def homepage():
-    return render_template("./index.html")
+    return render_template('index.html')
 
 @app.route('/<path:generatedKey>', methods=['GET'])
 def fetch_from_firebase(generatedKey):
-    ref = db.reference("/" + generatedKey)
+    ref = db.reference("/"+ generatedKey)
     data = ref.get()
     if not data:
         return '404 not found'
